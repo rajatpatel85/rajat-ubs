0085e1cd80eeab8ae9eb9c6f3d6d5a20b7494d0e
Rajat Ranjan Patel <37666246+rajatpatel85@users.noreply.github.com>
2018-08-18 17:33:26 -0500

    Added EnableDiscoveryClient annotations

--- customer/src/main/java/com/rrp/ubs/customer/CustomerApplication.java
+++ customer/src/main/java/com/rrp/ubs/customer/CustomerApplication.java
@@ -2,6 +2,7 @@ package com.rrp.ubs.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -15,6 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableDiscoveryClient
 public class CustomerApplication implements WebMvcConfigurer {
 	
 	public static void main( String[] args ){
