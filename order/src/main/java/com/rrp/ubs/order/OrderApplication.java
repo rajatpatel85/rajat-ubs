28d36156d73ac384a5dc9ef99f66e55dc49ab0af
Rajat Ranjan Patel <37666246+rajatpatel85@users.noreply.github.com>
2018-08-18 17:27:55 -0500

    Added Eureka Client annotations

--- order/src/main/java/com/rrp/ubs/order/OrderApplication.java
+++ order/src/main/java/com/rrp/ubs/order/OrderApplication.java
@@ -2,6 +2,7 @@ package com.rrp.ubs.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -15,6 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableDiscoveryClient
 public class OrderApplication implements WebMvcConfigurer {
 	
 	public static void main( String[] args ){
