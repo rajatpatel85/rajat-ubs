2b11ab07928c8f003169894e55e24f64994b1fa0
Rajat Ranjan Patel <37666246+rajatpatel85@users.noreply.github.com>
2018-08-18 17:25:46 -0500

    Added bootstrap.properties file and data.sql
    Added bootstrap.properties file to register the application name. Spring loads the bootstrap file before application properties file.
    Added data.sql file to initialize the h2 database tables.

--- order/src/main/resources/data.sql
+++ order/src/main/resources/data.sql
@@ -4,18 +4,33 @@
 
 ------ insert sample order data -----------
 
-insert into order_detail (order_id, order_code, order_description, order_create_date, order_notes, order_status, order_update_user_name) 
-values (1, 'REPAIR-PIPE', 'REPAIR OF HOUSE PIPE', now(), ' Order yet to start', 'INITIAL','ABC');
+insert into order_detail (id, order_id, customer_id, order_code, order_description, order_create_date, order_notes, order_status, order_update_user_name) 
+values (1, 1, 1,  'REPAIR-PIPE', 'REPAIR OF HOUSE PIPE', now(), ' Order yet to start', 'OPEN','ABC');
 
-insert into order_detail (order_id, order_code, order_description, order_create_date, order_notes, order_status, order_update_user_name) 
-values (2, 'REPAIR-PIPE', 'REPAIR OF HOUSE PIPE', now(), ' Order started by XYZ', 'STARTED','XYZ');
+insert into order_detail (id, order_id, customer_id, order_code, order_description, order_create_date, order_notes, order_status, order_update_user_name) 
+values (2, 1, 1, 'REPAIR-PIPE', 'REPAIR OF HOUSE PIPE', now(), ' Order started by XYZ', 'WORKING','XYZ');
 
-insert into order_detail (order_id, order_code, order_description, order_create_date, order_notes, order_status, order_update_user_name) 
-values (3, 'REPAIR-PIPE', 'REPAIR OF HOUSE PIPE', now(), ' Order COMPLETED', 'COMPLETED','der');
+insert into order_detail (id, order_id, customer_id, order_code, order_description, order_create_date, order_notes, order_status, order_update_user_name) 
+values (3, 1, 1, 'REPAIR-PIPE', 'REPAIR OF HOUSE PIPE', now(), ' Order COMPLETED', 'COMPLETED','der');
 
-insert into order_detail (order_id, order_code, order_description, order_create_date, order_notes, order_status, order_update_user_name) 
-values (4, 'REPAIR-GARAGE', 'REPAIR OF HOUSE GARAGE', now(), ' Order yet to start', 'INITIAL','ABC');
+insert into order_detail (id, order_id, customer_id, order_code, order_description, order_create_date, order_notes, order_status, order_update_user_name) 
+values (4, 1, 2, 'REPAIR-GARAGE', 'REPAIR OF HOUSE GARAGE', now(), ' Order yet to start', 'OPEN','ABC');
 
-insert into order_detail (order_id, order_code, order_description, order_create_date, order_notes, order_status, order_update_user_name) 
-values (5, 'REPAIR-KITCHEN', 'REPAIR OF HOUSE KITCHEN', now(), ' Order yet to start', 'INITIAL','ABC');
+insert into order_detail (id, order_id, customer_id, order_code, order_description, order_create_date, order_notes, order_status, order_update_user_name) 
+values (5, 2, 2, 'REPAIR-KITCHEN', 'REPAIR OF HOUSE KITCHEN', now(), ' Order started by XYZ', 'WORKING','ABC');
+
+insert into order_detail (id, order_id, customer_id, order_code, order_description, order_create_date, order_notes, order_status, order_update_user_name) 
+values (6, 2, 2, 'REPAIR-KITCHEN', 'REPAIR OF HOUSE KITCHEN', now(), ' Order started by XYZ', 'WORKING','ABC');
+
+insert into order_detail (id, order_id, customer_id, order_code, order_description, order_create_date, order_notes, order_status, order_update_user_name) 
+values (7, 3, 2, 'FIX-DOOR', 'REPAIR OF HOUSE KITCHEN', now(), ' Order started by XYZ', 'WORKING','ABC');
+
+insert into order_detail (id, order_id, customer_id, order_code, order_description, order_create_date, order_notes, order_status, order_update_user_name) 
+values (8, 3, 2, 'FIX-DOOR', 'REPAIR OF HOUSE KITCHEN', now(), ' Order started by XYZ', 'WORKING','ABC');
+
+insert into order_detail (id, order_id, customer_id, order_code, order_description, order_create_date, order_notes, order_status, order_update_user_name) 
+values (9, 4, 2, 'FIX-DOOR', 'REPAIR OF HOUSE KITCHEN', now(), ' Order started by XYZ', 'WORKING','ABC');
+
+insert into order_detail (id, order_id, customer_id, order_code, order_description, order_create_date, order_notes, order_status, order_update_user_name) 
+values (10, 4, 2, 'FIX-DOOR', 'REPAIR OF HOUSE KITCHEN', now(), ' Order started by XYZ', 'WORKING','ABC');
 
